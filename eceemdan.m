60b9e3c52711d0ab01c1f044051dd348f36851a2
Manuel Soto <132035245+Mimismo91@users.noreply.github.com>
2023-08-16 17:06:03 +0900

    Update eceemdan.m

--- eceemdan.m
+++ eceemdan.m
@@ -5,7 +5,7 @@
 % [modes, its] = eceemdan(x,Nstd,NR,MaxIter,SNRFlag,window_size,filt_method,tolerance, useParpool)
 %
 % x: input signal
-% Nstd: noise standard deviation
+% Nstd: noise standard deviation amplitude
 % NR: number of realizations
 % MaxIter: maximum number of iterations
 % SNRFlag: flag to adjust the noise amplitude
@@ -18,102 +18,33 @@
 % 'tolerance' must be > 0, once the 'tolerance' criterion has been meet the calculaiton will stop 
 %  
 %************************************ Noise filtering methods ******************************************************************************  
-%  'movmean' — Moving average over each window of A. This method is useful for reducing periodic trends in data.
+%  'movmean' — Moving average over each window of X. This method is useful for reducing periodic trends in data.
 % 
-%  'movmedian' — Moving median over each window of A. This method is useful for reducing periodic trends in data when outliers are present.
+%  'movmedian' — Moving median over each window of X. This method is useful for reducing periodic trends in data when outliers are present.
 % 
-%  'gaussian' — Gaussian-weighted moving average over each window of A.
+%  'gaussian' — Gaussian-weighted moving average over each window of X.
 % 
-%  'lowess' — Linear regression over each window of A. This method can be computationally expensive, but results in fewer discontinuities.
+%  'lowess' — Linear regression over each window of X. This method can be computationally expensive, but results in fewer discontinuities.
 % 
-%  'loess' — Quadratic regression over each window of A. This method is slightly more computationally expensive than 'lowess'.
+%  'loess' — Quadratic regression over each window of X. This method is slightly more computationally expensive than 'lowess'.
 % 
-%  'rlowess' — Robust linear regression over each window of A. This method is a more computationally expensive version of the method 'lowess', but it is more robust to outliers.
+%  'rlowess' — Robust linear regression over each window of X. This method is a more computationally expensive version of the method 'lowess', but it is more robust to outliers.
 % 
-%  'rloess' — Robust quadratic regression over each window of A. This method is a more computationally expensive version of the method 'loess', but it is more robust to outliers.
+%  'rloess' — Robust quadratic regression over each window of X. This method is a more computationally expensive version of the method 'loess', but it is more robust to outliers.
 % 
-%  'sgolay' — Savitzky-Golay filter, which smooths according to a quadratic polynomial that is fitted over each window of A. This method can be more effective than other methods when the data varies rapidly.
+%  'sgolay' — Savitzky-Golay filter, which smooths according to a quadratic polynomial that is fitted over each window of X. This method can be more effective than other methods when the data varies rapidly.
 %
 %
 % WARNING: for this code works it is necessary to include in the same
 % directoy the file emd.m developed by Rilling and Flandrin.
 % This file is available at http://perso.ens-lyon.fr/patrick.flandrin/emd.html
-
-
-% We use the default stopping criterion.
-% We use the last modification: 3.2007
-
-%   Syntax
-
-% modes=ceemdan(x,Nstd,NR,MaxIter,SNRFlag)
-% [modes its]=ceemdan(x,Nstd,NR,MaxIter,SNRFlag)
-
-%   Description
-
+% 
+% 
 % OUTPUT
 % modes: contain the obtained modes in a matrix with the rows being the modes        
 % its: contain the sifting iterations needed for each mode for each realization (one row for each realization)
-
-% INPUT
-% x: signal to decompose
-% Nstd: noise standard deviation
-% NR: number of realizations
-% MaxIter: maximum number of sifting iterations allowed.
-% SNRFlag: if equals 1, then the SNR increases for every stage, as in [1].
-%          if equals 2, then the SNR is the same for all stages, as in [2]. 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% The current is an improved version, introduced in:
-
-%[1] Colominas MA, Schlotthauer G, Torres ME. "Improve complete ensemble EMD: A suitable tool for biomedical signal processing" 
-%       Biomedical Signal Processing and Control vol. 14 pp. 19-29 (2014)
-
-%The original CEEMDAN algorithm was first introduced at ICASSP 2011, Prague, Czech Republic
-
-%The authors will be thankful if the users of this code reference the work
-%where the algorithm was first presented:
-
-%[2] Torres ME, Colominas MA, Schlotthauer G, Flandrin P. "A Complete Ensemble Empirical Mode Decomposition with Adaptive Noise"
-%       Proc. 36th Int. Conf. on Acoustics, Speech and Signa Processing ICASSP 2011 (May 22-27, Prague, Czech Republic)
-
-%Author: Marcelo A. Colominas
-%contact: macolominas@bioingenieria.edu.ar
-%Last version: 25 feb 2015
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% The current is a enhanced version of the impCEEMDAN. Last version: 28 jun 2023
-% Authors: Manuel Soto Calvo. manuel.sotocalvo@gmail.com; Han Soo Lee.leehs@hiroshima-u.ac.jp
-% Published in:
-%%****************************************************************************************************************************************
-% x: input signal
-% Nstd: noise standard deviation
-% NR: number of realizations
-% MaxIter: maximum number of iterations
-% SNRFlag: flag to adjust the noise amplitude
-% window_size: size of the moving window for the moving standard deviation
-% filt_method: filter method for smoothing the signal
-% tolerance: threshold for stopping criteria
-% useParpool: flag to decide if use parallel computation or not
-% 'windows_size' must be a 2-elements vector which means the number of elements before and after of the central element.
-% 'windows' must be > 0 && 'windows' < length (x - 1)
-% 'tolerance' must be > 0, once the 'tolerance' criterion has been meet the calculaiton will stop 
-%  
-%************************************ Noise filtering methods ******************************************************************************  
-%  'movmean' — Moving average over each window of A. This method is useful for reducing periodic trends in data.
+%
 % 
-%  'movmedian' — Moving median over each window of A. This method is useful for reducing periodic trends in data when outliers are present.
-% 
-%  'gaussian' — Gaussian-weighted moving average over each window of A.
-% 
-%  'lowess' — Linear regression over each window of A. This method can be computationally expensive, but results in fewer discontinuities.
-% 
-%  'loess' — Quadratic regression over each window of A. This method is slightly more computationally expensive than 'lowess'.
-% 
-%  'rlowess' — Robust linear regression over each window of A. This method is a more computationally expensive version of the method 'lowess', but it is more robust to outliers.
-% 
-%  'rloess' — Robust quadratic regression over each window of A. This method is a more computationally expensive version of the method 'loess', but it is more robust to outliers.
-% 
-%  'sgolay' — Savitzky-Golay filter, which smooths according to a quadratic polynomial that is fitted over each window of A. This method can be more effective than other methods when the data varies rapidly.
-%**************************************************************************************************************************************************************************************************************
 
 function [modes, its]= eceemdan(x,Nstd,NR,MaxIter,SNRFlag,window_size,filt_method,tolerance, useParpool)
 
